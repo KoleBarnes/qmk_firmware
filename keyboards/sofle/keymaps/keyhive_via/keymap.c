f3d4cacba838ac4cd8bd0580027aafcf686e800f
KoleBarnes <kbarnes261@gmail.com>
2021-06-23 09:26:32 -0700

    switched alt and ctrl. cleaned up raise layer and added home and end keys.
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- keyboards/sofle/keymaps/keyhive_via/keymap.c
+++ keyboards/sofle/keymaps/keyhive_via/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Mute  |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | LGUI |LOWER | / Bspc  /       \Space \  |RAISE |      |  [   |  ]   |
+ *            | LALT | LGUI |LCTRL |LOWER | / Bspc  /       \Space \  |RAISE |      |  [   |  ]   |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *             `----------------------------------'           '------''---------------------------'
  */
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   KC_GRV,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-              KC_LCTRL, KC_LGUI, KC_LALT, MO(2), KC_BSPC,            KC_SPC,  MO(3), XXXXXXX, KC_LBRACKET, KC_RBRACKET
+              KC_LALT, KC_LGUI, KC_LCTRL, MO(2), KC_BSPC,            KC_SPC,  MO(3), XXXXXXX, KC_LBRACKET, KC_RBRACKET
 ),
 /*
  * COLEMAK
@@ -75,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  `   |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  | Shift|
+ * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  | Enter|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | LGUI |LOWER | / Bspc  /       \Space \  |RAISE |      |  [   |  ]   |
+ *            | LALT | LGUI |LCTRL |LOWER | / Bspc  /       \Space \  |RAISE |      |  [   |  ]   |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *             `----------------------------------'           '------''---------------------------'
  */
@@ -90,23 +90,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |  1   |   2  |   3  |   4  |  5   |                    |   6  |   7  |   8  |   9  |   0  |   -  |
+ * |      |      |      |      |RGBTOG|RGBVAI|                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  | Ins  | Pscr | Menu |      |      |                    | PGUP |   U  |  Up  |  O   | DLine| Bspc |
+ * |      |      |      |      |      |      |                    | PGUP | Home |  Up  | End  |      | Del  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | `    | LAt  | LCtl |LShift|      | Caps |-------.    ,-------| PGDN | Left | Down |Right |  Del | Bspc |
+ * |      |      |      |      |      | Caps |-------.    ,-------| PGDN | Left | Down |Right |      | Bspc |
  * |------+------+------+------+------+------| MUTE  |    |       |------+------+------+------+------+------|
- * |Shift | Undo |  Cut | Copy | Paste|      |-------|    |-------|      |  M   |      |  .   |      | Shift|
+ * |Shift |      |      |      |      |      |-------|    |-------|      |      |      |      |      | Enter|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | LGUI |LOWER | / Bspc  /       \Space \  |RAISE |      |  [   |  ]   |
+ *            | LALT | LGUI |LCTRL |LOWER | / Bspc  /       \Space \  |RAISE |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *             `----------------------------------'           '------''---------------------------'
  */
 [3] = LAYOUT(
-  _______, _______ , _______ , _______ , RGB_TOG , RGB_VAI,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
-  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, _______,   KC_UP, _______,_______, KC_BSPC,
-  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  XXXXXXX, _______, XXXXXXX, _______,   XXXXXXX, _______,
-                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG , RGB_VAI,                           XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX,                        KC_PGUP, KC_HOME,   KC_UP, KC_END,XXXXXXX, _______,
+  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  XXXXXXX, KC_BSPC,
+  _______,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______,       _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______,
+                         _______, _______, _______, _______, _______,       _______, _______, _______, XXXXXXX, XXXXXXX
 )
 };
