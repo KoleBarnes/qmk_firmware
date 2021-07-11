e0069c52c5a64db9beff40d01848351aebce52bd
KoleBarnes <kbarnes261@gmail.com>
2021-07-11 12:38:06 -0700

    Moved square brackets to 9 and 0 on raise layer. Added num layer (commented out). Shifted alt and gui keys over one on left half. Added mod-tap to tap enter hold shift. Fixed game layer getting stuck on.
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- keyboards/sofle/keymaps/keyhive_via/keymap.c
+++ keyboards/sofle/keymaps/keyhive_via/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   KC_GRV,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-              KC_LALT, KC_LGUI, XXXXXXX, KC_LCTRL, LT(2, KC_BSPC),            LT(3, KC_SPC),  KC_ENT, XXXXXXX, KC_LBRACKET, KC_RBRACKET
+              XXXXXXX, KC_LALT, KC_LGUI, KC_LCTRL, LT(2, KC_BSPC),            LT(3, KC_SPC),  LSFT_T(KC_ENT), XXXXXXX, XXXXXXX, XXXXXXX
 ),
 /*
  * Game
@@ -69,30 +69,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX,   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   XXXXXXX,   XXXXXXX,   KC_A,    KC_S,    KC_D,    KC_F,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   XXXXXXX,  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,     ,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-              KC_LALT, KC_LGUI, KC_LCTRL, XXXXXXX, KC_SPC,            KC_SPC,  KC_TRNS, KC_TRNS, KC_LBRACKET, KC_RBRACKET
+              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
-// /*
-//  * COLEMAK
-//  * ,-----------------------------------------.                    ,-----------------------------------------.
-//  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
-//  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-//  * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
-//  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-//  * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
-//  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
-//  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
-//  * `-----------------------------------------/       /     \      \-----------------------------------------'
-//  *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
-//  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
-//  *             `----------------------------------'           '------''---------------------------'
-//  */
-
+/*
+ * COLEMAK
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *             `----------------------------------'           '------''---------------------------'
+ */
 // [1] = LAYOUT(
 //   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
 //   KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
 //   KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
 //   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
 //                  KC_LGUI,KC_LALT,KC_LCTRL,KC_TRNS, KC_ENT,      KC_SPC,  KC_TRNS, KC_RCTRL, KC_RALT, KC_RGUI
+// ),
+/*
+ * NUM PAD
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *             `----------------------------------'           '------''---------------------------'
+ */
+// [2] = LAYOUT(
+//   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+//   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      XXXXXXX,   KC_1,    KC_2,    KC_3,XXXXXXX,XXXXXXX,
+//   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      XXXXXXX,    KC_4,    KC_5,    KC_6,XXXXXXX,XXXXXXX,
+//   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,      XXXXXXX,XXXXXXX,    KC_7, KC_8,  KC_9,XXXXXXX,XXXXXXX,
+//                  _______,_______,_______,_______,_______,      _______,  _______, KC_0,_______,_______
 // ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -130,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *             `----------------------------------'           '------''---------------------------'
  */
 [3] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG , RGB_VAI,                           TG(1),  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG , RGB_VAI,                           TG(1),  XXXXXXX, XXXXXXX, KC_LBRACKET, KC_RBRACKET, XXXXXXX,  
   XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX,                        KC_PGUP, KC_HOME,   KC_UP, KC_END,XXXXXXX, _______,
   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  XXXXXXX, KC_BSPC,
   _______,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______,       _______,  XXXXXXX, LGUI(LCTL(KC_LEFT)), XXXXXXX, LGUI(LCTL(KC_RGHT)),   XXXXXXX, _______,
