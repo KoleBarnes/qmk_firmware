0cb57fb3d5636f72fb409721a9a3deacbf25aa5f
KoleBarnes <kbarnes261@gmail.com>
2021-09-03 19:12:12 -0700

    encoder function data type update.
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- keyboards/sofle/keymaps/keyhive_via/encoder.c
+++ keyboards/sofle/keymaps/keyhive_via/encoder.c
@@ -19,20 +19,21 @@
 
 #ifdef ENCODER_ENABLE
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
         if (clockwise) {
             tap_code(KC_VOLU);
         } else {
             tap_code(KC_VOLD);
         }
-    } else if (index == 1) {
+    } else if (index == 1) { /* Second encoder */
         if (clockwise) {
             tap_code(KC_PGDOWN);
         } else {
             tap_code(KC_PGUP);
         }
     }
+    return true;
 }
 
 #endif
