281cdfb3cc6491f273fded1209a131b1b3d622d2
KoleBarnes <kole@neoterictech.ca>
2024-01-23 14:36:44 -0800

    Sofle Advanced keymap: Removed Home-row mods. Made right half transparent on game layer
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- keyboards/sofle/keymaps/advanced/keymap.c
+++ keyboards/sofle/keymaps/advanced/keymap.c
@@ -59,20 +59,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  VolUp   |  < N >  |  Pg Up    |------+------+------+------+------+------|
  * | #/Tab|   Q  |   W  |   E  |   R  |   T  |----------|    C    |-----------|   Y  |   U  |   I  |   O  |   P  | Del  |
  * |------+------+------+------+------+------|  Mute    |  < O >  |           |------+------+------+------+------+------|
- * | APP  |   A  | ALT/S| CTL/D| GUI/F| LWR/G|----------|    D    |-----------| RAS/H| GUI/J| CTR/K| ALT/L|   ;  |  '   |
+ * | APP  |   A  |   S  |   D  |   F  |   G  |----------|    D    |-----------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  VolDn   |  < E >  |  Pg Dn    |------+------+------+------+------+------|
  * |SpcCad|   Z  |   X  |   C  |   V  |   B  |----------|    R    |-----------|   N  |   M  |   ,  |   .  |   /  |SpcCad|
  * '-----------------------------------------|  /       /         \       \   |-----------------------------------------'
- *               | LEAD | LALT |LSUPER|LCTRL | / LOWER /           \ RAISE \  |RSHIFT| RCLT | RALT | LEAD |
+ *               | LEAD | LALT |LSUPER|LCTRL | / LOWER /           \ RAISE \  |RSHIFT|LSUPER| RALT | LEAD |
  *               |      |      |      |      |/ SPACE /             \ BSPC  \ |ENTER |      |      |      |
  *               '-----------------------------------'               '------------------------------------'
  */
 [_QWERTY] = LAYOUT(
-  KC_ESC            , KC_1   , KC_2              , KC_3              , KC_4              , KC_5            ,                                              KC_6             , KC_7              , KC_8              , KC_9              , KC_0   , KC_MINUS,
-  LT(_NUMPAD,KC_TAB), KC_Q   , KC_W              , KC_E              , KC_R              , KC_T            ,                                              KC_Y             , KC_U              , KC_I              , KC_O              , KC_P   ,   KC_DEL,
-  KC_APP            , KC_A   , MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LGUI, KC_F), LT(_LOWER, KC_G),                                              LT(_RAISE, KC_H ), MT(MOD_LGUI, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_RALT, KC_L), KC_SCLN,  KC_QUOT,
-  SC_LSPO           , KC_Z   , KC_X              , KC_C              , KC_V              , KC_B            , KC_MUTE           ,     XXXXXXX            , KC_N             , KC_M              , KC_COMM           , KC_DOT            , KC_SLSH,  SC_RSPC,
-                               QK_LEAD           , KC_LALT           , KC_LGUI           , KC_LCTL         , LT(_LOWER, KC_SPC),     LT(_RAISE, KC_BSPC), RSFT_T(KC_ENT)   , KC_RGUI           , KC_RALT           , QK_LEAD
+  KC_ESC            , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                              KC_6          , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINUS,
+  LT(_NUMPAD,KC_TAB), KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                              KC_Y          , KC_U   , KC_I   , KC_O   , KC_P   ,   KC_DEL,
+  KC_APP            , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                              KC_H          , KC_J   , KC_K   , KC_L   , KC_SCLN,  KC_QUOT,
+  SC_LSPO           , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE           ,     XXXXXXX            , KC_N          , KC_M   , KC_COMM, KC_DOT , KC_SLSH,  SC_RSPC,
+                               QK_LEAD, KC_LALT, KC_LGUI, KC_LCTL, LT(_LOWER, KC_SPC),     LT(_RAISE, KC_BSPC), RSFT_T(KC_ENT), KC_LGUI, KC_RALT, QK_LEAD
 ),
 
 
@@ -101,23 +101,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Game
  * .-----------------------------------------.                                .-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |----------.    E    .-----------|   6  |   7  |   8  |   9  |   0  |  -   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |----------.    E    .-----------|______|______|______|______|______|______|
  * |------+------+------+------+------+------|  ______  |  < N >  |  ______   |------+------+------+------+------+------|
- * |   `  | Tab  |   Q  |   W  |   R  |   T  |----------|    C    |-----------|   Y  |   U  |   I  |   O  |   P  | Del  |
+ * |   `  | Tab  |   Q  |   W  |   R  |   T  |----------|    C    |-----------|______|______|______|______|______|______|
  * |------+------+------+------+------+------|  ______  |  < O >  |  ______   |------+------+------+------+------+------|
- * |   G  |      |   A  |   S  |   F  |   G  |----------|    D    |-----------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |   G  |      |   A  |   S  |   F  |   G  |----------|    D    |-----------|______|______|______|______|______|______|
  * |------+------+------+------+------+------|  ______  |  < E >  |  ______   |------+------+------+------+------+------|
- * |      |      |   Z  |   X  |   V  |   B  |----------|    R    |-----------|   N  |   M  |   ,  |   .  |   /  |______|
+ * |      |      |   Z  |   X  |   V  |   B  |----------|    R    |-----------|______|______|______|______|______|______|
  * '-----------------------------------------|  /       /         \       \   |-----------------------------------------'
  *               |      |      |      |      | / SPACE /           \ ______\  |______|______|______|______|
  *               |      |      |      |      |/       /             \       \ |      |      |      |      |
  *               '-----------------------------------'               '------------------------------------'
 */
 [_GAME] = LAYOUT(
-  KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                       KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINUS,
-  KC_GRV , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,                       KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   ,   KC_DEL,
-  KC_G   , KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   ,                       KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN,  KC_QUOT,
-  XXXXXXX, KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , _______,     XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,  _______,
+  KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                       _______, _______, _______, _______, _______, _______,
+  KC_GRV , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,                       _______, _______, _______, _______, _______, _______,
+  KC_G   , KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   ,                       _______, _______, _______, _______, _______, _______,
+  XXXXXXX, KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , _______,     _______, _______, _______, _______, _______, _______, _______,
                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_SPC,     _______, _______, _______, _______, _______
 ),
 
